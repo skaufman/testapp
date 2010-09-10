2aecbd0b31e4055d5e46c52a57b134002d90a89e
Stan Kaufman <skaufman@epimetrics.com>
2010-09-10 14:42:36 -0700

    added rest of stock app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '743305bff1e8a8e42bf288eb22d1a50f2d3f4745a825032ddd00e366e988f16e1d5ba8c0e94027b4b943c97e0bfc6a1c57490aacefad6ac747caa1994e474b6d'
