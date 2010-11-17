a6f8c7c534aab27aa12518f9416a97bb8ffce0b1
Stan Kaufman <skaufman@epimetrics.com>
2010-11-17 10:03:57 -0800

    added admin namespace for users controller

--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,10 @@ Testapp::Application.routes.draw do
   #     resources :products
   #   end
 
+  namespace :admin do
+    resources :users
+  end
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 #  root :to => "home#index"
